603c4a51d087ef1115fe3dd35fe6294b429331cc
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-13 14:07:57 +0530

    client- src- theme.js- Code with light and dark mode.

--- client/src/theme.js
+++ client/src/theme.js
@@ -119,3 +119,76 @@ export const tokens = (mode) => ({
         },
       }),
 });
+
+// mui theme settings
+export const themeSettings = (mode) => {
+    const colors = tokens(mode);
+    return {
+      palette: {
+        mode: mode,
+        ...(mode === "dark"
+          ? {
+              // palette values for dark mode
+              primary: {
+                main: colors.primary[500],
+              },
+              secondary: {
+                main: colors.greenAccent[500],
+              },
+              neutral: {
+                dark: colors.grey[700],
+                main: colors.grey[500],
+                light: colors.grey[100],
+              },
+              background: {
+                default: colors.primary[500],
+              },
+            }
+          : {
+              // palette values for light mode
+              primary: {
+                main: colors.primary[100],
+              },
+              secondary: {
+                main: colors.greenAccent[500],
+              },
+              neutral: {
+                dark: colors.grey[700],
+                main: colors.grey[500],
+                light: colors.grey[100],
+              },
+              background: {
+                default: "#fcfcfc",
+              },
+            }),
+      },
+      typography: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 12,
+        h1: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 40,
+        },
+        h2: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 32,
+        },
+        h3: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 24,
+        },
+        h4: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 20,
+        },
+        h5: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 16,
+        },
+        h6: {
+          fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+          fontSize: 14,
+        },
+      },
+    };
+  };

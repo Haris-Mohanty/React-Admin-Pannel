010bffb0b8bcfb9f5f50e987a52cee750d883be0
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-14 18:20:48 +0530

    client- src- scenes- global- Topbar.jsx- Search bar added.

--- client/src/scenes/global/Topbar.jsx
+++ client/src/scenes/global/Topbar.jsx
@@ -15,7 +15,18 @@ const Topbar = () => {
   const colorMode = useContext(ColorModeContext);
   return;
   <Box display="flex" justifyContent="space-between" p={2}>
-    <IconButton></IconButton>
+    {/* SEARCH BAR */}
+    <Box
+      display="flex"
+      backgroundColor={colors.primary[400]}
+      borderRadius="3px"
+    >
+      <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+      <IconButton type="button" sx={{ p: 1 }}>
+        <SearchIcon />
+      </IconButton>
+    </Box>
+    {/* ICONS */}
   </Box>;
 };
 

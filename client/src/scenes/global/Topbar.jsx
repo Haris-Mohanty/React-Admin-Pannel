0f6c5a89e6b7a395a720a6d16a9fd0397c6ea00c
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-14 18:37:21 +0530

    client- src- scenes- global- Topbar.jsx- Code fixed.

--- client/src/scenes/global/Topbar.jsx
+++ client/src/scenes/global/Topbar.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
 import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
@@ -13,42 +13,42 @@ const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
-  return;
-  <Box display="flex" justifyContent="space-between" p={2}>
-    {/* SEARCH BAR */}
-    <Box
-      display="flex"
-      backgroundColor={colors.primary[400]}
-      borderRadius="3px"
-    >
-      <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-      <IconButton type="button" sx={{ p: 1 }}>
-        <SearchIcon />
-      </IconButton>
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
     </Box>
-    {/* ICONS */}
-    <Box display="flex">
-      <IconButton onClick={colorMode.toggleColorMode}>
-        {theme.palette.mode === "dark" ? (
-          <DarkModeOutlinedIcon />
-        ) : (
-          <LightModeOutlinedIcon />
-        )}
-      </IconButton>
-
-      <IconButton>
-        <NotificationsOutlinedIcon />
-      </IconButton>
-
-      <IconButton>
-        <SettingsOutlinedIcon />
-      </IconButton>
-
-      <IconButton>
-        <PersonOutlinedIcon />
-      </IconButton>
-    </Box>
-  </Box>;
+  );
 };
 
 export default Topbar;

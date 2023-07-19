10e3e982bcb28bd4bd7145d51b5a621cbb6a059b
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-19 09:50:54 +0530

    client- src- sense- dashboard- index.jsx- Dashboard page(homepage) code processing..

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box m={"20px"}>
+      <Box
+      <Header title={"DASHBOARD"} subtitle={"Welcome to your Dashboard!"} />
+    </Box>
+  );
+};
 
-export default Dashboard
+export default Dashboard;

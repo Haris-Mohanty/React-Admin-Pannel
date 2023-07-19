bcdb9259701bc1f9cd896f012a9a4dfa9122da90
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-19 10:52:36 +0530

    client- src- sense- team- Team.jsx- Show team code processing......

--- client/src/scenes/team/Team.jsx
+++ client/src/scenes/team/Team.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
 
 const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box>
+      <Header title={"TEAM"} subtitle={"Managing the Team Members"} />
+    </Box>
+  );
+};
 
-export default Team
+export default Team;

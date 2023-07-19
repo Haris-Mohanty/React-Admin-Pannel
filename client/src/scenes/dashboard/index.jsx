81f56dd7c3f8b921f3a7a2e1c3e026e0bfc16929
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-19 09:52:00 +0530

    client- src- sense- dashboard- index.jsx- Showing title and subtitle successfully.

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -6,7 +6,12 @@ const Dashboard = () => {
   return (
     <Box m={"20px"}>
       <Box
-      <Header title={"DASHBOARD"} subtitle={"Welcome to your Dashboard!"} />
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Header title={"DASHBOARD"} subtitle={"Welcome to your Dashboard!"} />
+      </Box>
     </Box>
   );
 };

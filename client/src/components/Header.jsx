a14d2d2b68f05d60b92dada6f21a93eb8806086f
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-19 09:45:35 +0530

    client- src- components- Header.jsx- Make a  dynamic header coder pocessing...

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography, Box, useTheme} from "@mui/material"
+import { tokens } from '../theme'
+
+const Header = ({title, subtitle}) => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header

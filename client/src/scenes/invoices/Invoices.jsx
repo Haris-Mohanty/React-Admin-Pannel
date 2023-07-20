4150889ad3cbffd62cf06d4a6371e3a5fb1cdb2e
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-20 14:56:29 +0530

    client- src- sense- invoices- Invoices.jsx- Show invoice balance list in table code processing...

--- client/src/scenes/invoices/Invoices.jsx
+++ client/src/scenes/invoices/Invoices.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Typography, Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
 
 const Invoices = () => {
-  return (
-    <div>
-      <h2>Haris</h2>
-    </div>
-  )
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "id", headerName: "ID" },
+  ];
 
-export default Invoices
+  return (
+    <Box m={"20px"}>
+      <Header title={"INVOICES"} subtitle={"List of Invoice Balances"} />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Invoices;

ac2f842404acee68de1dea5d70a4ad0e15c3eb79
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-20 16:54:07 +0530

    client- src- sense- form- Form.jsx- form creating...

--- client/src/scenes/form/Form.jsx
+++ client/src/scenes/form/Form.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+
 const Form = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <Box m={"20px"}>
       <Header title={"CREATE USER"} subtitle={"Create a new user profile."} />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      ></Formik>
     </Box>
   );
 };

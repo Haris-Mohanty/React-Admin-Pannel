bc595a251db4f29a58e555e232f38a9bbf81a890
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-20 14:58:05 +0530

    client- src- sense- invoices- Invoices.jsx- Show invoice balance list in table code fixed.

--- client/src/scenes/invoices/Invoices.jsx
+++ client/src/scenes/invoices/Invoices.jsx
@@ -10,6 +10,37 @@ const Invoices = () => {
   const colors = tokens(theme.palette.mode);
   const columns = [
     { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
   ];
 
   return (

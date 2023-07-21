d9487f7e939272b05fad554e231215c126117e00
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-21 13:39:34 +0530

    client- src- sense- calendar- Calendar.jsx- Imported all data.

--- client/src/scenes/calendar/Calendar.jsx
+++ client/src/scenes/calendar/Calendar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
 const Calendar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Calendar
+export default Calendar;

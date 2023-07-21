28e1fcdeac693f59810b5928fdb71cb483ec4fdc
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-21 14:08:43 +0530

    client- src- sense- faq- FAQ.JSX- Add frequently asked question code processing.....

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,10 @@ import Contacts from "./scenes/contacts/Contacts";
 import Invoices from "./scenes/invoices/Invoices";
 import Form from "./scenes/form/Form";
 import Calendar from "./scenes/calendar/Calendar";
+import FAQ from "./scenes/faq/FAQ";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -33,10 +33,10 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>

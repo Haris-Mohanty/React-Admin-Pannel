daf7678171c1e1a9dbbba683fd1d0f47accbdae8
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-21 13:30:52 +0530

    client- src- sense- calendar- Calendar.jsx- Show calander code processing.....

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,12 @@ import Team from "./scenes/team/Team";
 import Contacts from "./scenes/contacts/Contacts";
 import Invoices from "./scenes/invoices/Invoices";
 import Form from "./scenes/form/Form";
+import Calendar from "./scenes/calendar/Calendar";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
@@ -32,12 +32,12 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
---
+++ client/src/scenes/calendar/Calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Calendar

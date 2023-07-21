a8847e1a471cfdc86ca2df0e965916c157adc9f7
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-21 14:14:10 +0530

    client- src- sense- faq- FAQ.JSX- Imported every thing needed.

--- client/src/scenes/faq/FAQ.jsx
+++ client/src/scenes/faq/FAQ.jsx
@@ -1,8 +1,16 @@
-import { Box } from "@mui/material";
 import React from "react";
+import { Box, useTheme } from "@mui/material";
 import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
 
 const FAQ = () => {
+  const theme = useTheme();
+  const tokens = tokens(theme.palette.mode);
   return (
     <Box m={"20px"}>
       <Header title={"FAQ"} subtitle={"Frequently Asked Questions Page."} />

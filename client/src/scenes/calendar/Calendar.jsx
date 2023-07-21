5c6082d554d9b6e9176b1016b7438aeec12b93dd
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-21 13:42:17 +0530

    client- src- sense- calendar- Calendar.jsx- Add header successfully.

--- client/src/scenes/calendar/Calendar.jsx
+++ client/src/scenes/calendar/Calendar.jsx
@@ -17,7 +17,13 @@ import Header from "../../components/Header";
 import { tokens } from "../../theme";
 
 const Calendar = () => {
-  return <div></div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m={"20px"}>
+      <Header title={"Calendar"} subtitle={"Full Calendar Interactive Page."} />
+    </Box>
+  );
 };
 
 export default Calendar;

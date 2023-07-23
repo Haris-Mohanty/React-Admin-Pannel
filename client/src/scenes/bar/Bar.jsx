180b61cd3e47626659f5b71c929a67d97a8034d5
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-23 13:23:29 +0530

    client- src- components- BarChart.jsx- Bar chart created successfully.

--- client/src/scenes/bar/Bar.jsx
+++ client/src/scenes/bar/Bar.jsx
@@ -7,7 +7,7 @@ const Bar = () => {
   return (
     <Box m={"20px"}>
       <Header title={"Bar Chart"} subtitle={"Simple Bar Chart."} />
-      <Box height={"70vh"}>
+      <Box height={"75vh"}>
         <BarChart />
       </Box>
     </Box>

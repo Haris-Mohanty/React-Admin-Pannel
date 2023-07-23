01cb362d0e6c25e859c6a616d81bd0512c52fcfc
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-23 13:47:39 +0530

    client- src- components- LineChart.jsx- Create line chart code processing...

---
+++ client/src/components/LineChart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ResponsiveLine } from "@nivo/line";
+import { useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const LineChart = () => {
+  return <div></div>;
+};
+
+export default LineChart;

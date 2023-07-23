a5c6e21c36e8a5d0738cda6b699045edf8db955d
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-23 13:29:59 +0530

    client- src- components- PieChart.jsx- Show pie chart create...

--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {useTheme} from "@mui/material"
+import { tokens } from '../theme'
+import {mockBarData as data} from "../data/mockData"
+
 
 const PieChart = () => {
+    const theme  = useTheme();
+    const colors = tokens(theme.palette.mode)
   return (
     <div>
       

36f01ceb58ede79869c7d82b4b0c66e18117fca6
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-23 13:50:33 +0530

    client- src- sense- line- Line.jsx- Line chart showing successfully.

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -4,7 +4,7 @@ import { useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import { mockLineData as data } from "../data/mockData";
 
-const LineChart = () => {
+const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
--- client/src/scenes/line/Line.jsx
+++ client/src/scenes/line/Line.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
 
 const Line = () => {
   return (
     <Box m={"20px"}>
       <Header title={"Line Chart"} subtitle={"Simple Line Chart."} />
+      <Box height={"75vh"}>
+        <LineChart />
+      </Box>
     </Box>
   );
 };

31fa9d7909ad2aa525315953ed6a0e89f2e4b8cc
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-24 19:15:31 +0530

    client- src- sense- geography- Geography.jsx- Header added successfully.

--- client/src/scenes/geography/Geography.jsx
+++ client/src/scenes/geography/Geography.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 const Geography = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box m={"20px"}>
+      <Header title={"Geography Chart"} subtitle={"Simple Geography Chart."} />
+    </Box>
+  );
+};
 
-export default Geography
+export default Geography;

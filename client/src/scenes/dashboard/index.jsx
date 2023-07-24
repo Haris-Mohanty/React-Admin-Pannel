a567aa7843b72076e2d98f488e3e78d865c7cf8a
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-24 19:58:11 +0530

    client- src- sense- dashboard- index.jsx- Add a button download reports successfully.

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import Header from "../../components/Header";
 import { Box, Button, IconButton, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <Box m={"20px"}>
       <Box
@@ -12,6 +20,21 @@ const Dashboard = () => {
         alignItems={"center"}
       >
         <Header title={"DASHBOARD"} subtitle={"Welcome to your Dashboard!"} />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
       </Box>
     </Box>
   );

bfdbdaec9388e764ba66183dc1e7d22bf66702cf
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-24 19:44:02 +0530

    client- src- components- StatBox.jsx- ProgressCircle imported.

--- client/src/components/StatBox.jsx
+++ client/src/components/StatBox.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useTheme, Typography, Box } from '@mui/material'
+import { tokens } from '../theme';
+import ProgressCircle from "./ProgressCircle"
 
 const StatBox = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
   return (
     <div>
       

fb41041d19bb6e96417f6041f16377b79595539b
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-24 19:18:13 +0530

    client- src- components- GeographyChart.jsx- Create Geography chat code processing.....

--- client/src/components/GeographyChart.jsx
+++ client/src/components/GeographyChart.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useTheme} from "@mui/material";
 import { tokens } from '../theme'
+import { mockGeographyData as data } from "../data/mockData";
 
 const GeographyChart = () => {
   return (
